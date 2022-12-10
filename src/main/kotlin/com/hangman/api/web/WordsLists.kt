1c7d07df2dc7eceaf0878eff511cb4f2a03fafc5
Pau Garcia Gozalvez <pgarcia@ledmotive.com>
2022-12-10 17:06:40 +0100

    Paths are relative to be found from .jar

--- src/main/kotlin/com/hangman/api/web/WordsLists.kt
+++ src/main/kotlin/com/hangman/api/web/WordsLists.kt
@@ -5,42 +5,39 @@ import java.text.Normalizer
 
 object WordsLists {
 
-    private const val EN_WORDS_FILE = "words_en.txt"
-    private const val CAT_WORDS_FILE = "words_cat.txt"
-    private const val ES_WORDS_FILE = "words_es_complete.txt"
+    private const val EN_WORDS_FILE = "/words_en.txt"
+    private const val CAT_WORDS_FILE = "/words_cat.txt"
+    private const val ES_WORDS_FILE = "/words_es.txt"
 
     enum class Language(val code: String) {
         EN("en"), CAT("cat"), ES("es")
     }
 
-    val english: ArrayList<String>
-    val catala: ArrayList<String>
-    val spanish: ArrayList<String>
+    val english: List<String>
+    val catala: List<String>
+    val spanish: List<String>
 
     init {
-        val path = System.getProperty("user.dir") + File.separator
-
-        english = loadWordsFromFile(path + EN_WORDS_FILE)
-        catala = loadWordsFromFile(path + CAT_WORDS_FILE)
-        spanish = loadWordsFromFile(path + ES_WORDS_FILE)
+        english = loadWordsFromFile(javaClass.getResourceAsStream(EN_WORDS_FILE))
+        catala = loadWordsFromFile(javaClass.getResourceAsStream(CAT_WORDS_FILE))
+        spanish = loadWordsFromFile(javaClass.getResourceAsStream(ES_WORDS_FILE))
     }
 
-    private fun loadWordsFromFile(file: String): ArrayList<String> {
-        val collection = ArrayList<String>()
+    private fun loadWordsFromFile(file: InputStream?): List<String> {
         try {
-            BufferedReader(FileReader(file)).use {
-                var word = it.readLine()
-                while (word != null) {
-                    collection.add(word)
-                    word = it.readLine()
-                }
+            if (file == null)
+                throw FileNotFoundException()
+
+            file.bufferedReader().use {
+                return it.readLines()
             }
         } catch (e: FileNotFoundException) {
+            System.err.println("File not found: $file")
             e.printStackTrace()
         } catch (e: IOException) {
             e.printStackTrace()
         }
-        return collection
+        return emptyList() // on error
     }
 
     fun CharSequence.unaccent(): String {
